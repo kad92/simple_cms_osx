2a37ba37995cfe8b04856240a97016cefab08541
kad92 <kadiatou_diallo@hotmail.com>
2019-08-09 15:12:10 -0700

    Add feature to add a subject

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -10,11 +10,31 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new({:name => "default"})
   end
 
   def edit
   end
 
+  def create
+    @subject = Subject.new(params.require(:subject).permit(:name,:position,:visible))
+
+    if @subject.save
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
+
+  end
+
   def delete
   end
+
+  private
+    def subject_params
+      #same as using "params[:subject]",
+      # - raises an error if :subject is not present
+      # - allows listed attributes to be mass-assigned
+      params.require(:subject).permit(:name,:position,:visible)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'subjects/index'
-  get 'subjects/show'
-  get 'subjects/new'
-  get 'subjects/edit'
-  get 'subjects/delete'
-
   root "subjects#index"
+  match ':controller(/:action(/:id))', :via => [:get,:post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
